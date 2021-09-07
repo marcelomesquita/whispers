61a07c4da9156ce3673895173b71b657d160ffe0
Marcelo Mesquita <contato@marcelomesquita.com>
2021-09-06 22:25:30 -0300

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../assets/styles/globals.scss'
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="author" content="Marcelo Mesquita" />
+        <meta name="description" content="" />
+
+        <title>Whispers</title>
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
-
-export default MyApp
